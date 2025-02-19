35e9f6f094530fd991fdfe5fbf169453b2b5d74a
prunesga <prunesga@student.21-school.ru>
2025-02-19 21:50:01 +0300

    05 edited

--- src/05/analyze.sh
+++ src/05/analyze.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+sort_by_status() {
+    local log_file="$1"
+    cat $log_file | awk '{print}' | sort -k9,9n
+}
+
+unique_ips() {
+    local log_file="$1"
+    cat $log_file | awk '{print $1}' | uniq
+}
+
+error_requests() {
+    local log_file="$1"
+    cat $log_file | awk '($9 >= 400 && $9 < 600) {print}' | uniq
+}
+
+error_ips() {
+    local log_file="$1"
+    cat $log_file | awk '($9 >= 400 && $9 < 600)' | awk '{print $1}' | sort | uniq
+}
